dbda8ebe29c85ab10b64adca7ef216b7c0476cd9
anmeng-sd <441414288@qq.com>
2014-09-26 16:52:34 +0800

    day

---
+++ src/build.lua
@@ -0,0 +1,8 @@
+skynet = require "skynet"
+local buildings = {}
+
+local function command.BUILD(building)
+end
+
+local function command.UPGRAD(building)
+end
--- src/build_conf.lua
+++ src/build_conf.lua
@@ -1,1 +1,18 @@
-base = {name = ¨¨}
+return {
+
+base = {name = "大本营"
+	level1 = {
+		hp = 1000
+		defense = 20
+		buildtime = 30
+	}
+
+	level2 = {
+		hp = 2000
+		defense = 200
+		buildtime = 300
+	}
+}
+
+}
+
