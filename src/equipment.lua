cf54897527a6c11ca752782399f691aa4cdd3aaa
anmeng-sd <441414288@qq.com>
2014-10-14 19:47:05 +0800

    daily commit

--- src/equipment.lua
+++ src/equipment.lua
@@ -1,16 +1,16 @@
+local equipment_conf = require "equipment_conf"
 local equipment_mgr = {
-	position = -1，
-	NECKLACE = 1,
-	LEFTRING = 2，
-	RIGHTRING= 3，
-	SHOE= 4，
-	BELT= 5，
-	HELMET = 6，
-	DRESS= 7，
-	WEAPON = 8
 }
 --get a new equipment
 local function equipment_mgr.new(id)
-	
+	local t =  {}
+	local equipment_model = equipment_conf[id]
+	t.position = equipment_model.position
+	if equipment_model.hp then t.hp = equipment_model.hp end
+	return t
+end
+
+local function equipment_mgr:add_actor(actor)
+	-- body
 end
 
--- src/equipment_conf.lua
+++ src/equipment_conf.lua
@@ -1,3 +1,19 @@
 local equipment_conf = {
 	
+	NECKLACE = 1,
+	LEFTRING = 2，
+	RIGHTRING= 3，
+	SHOE= 4，
+	BELT= 5，
+	HELMET = 6，
+	DRESS= 7，
+	WEAPON = 8,
+	classic_cloth = {
+	position = NECKLACE，
+	name = '布衣(男)',
+	job = 0,
+	hp = 100
+	}
+}
+	
 }
